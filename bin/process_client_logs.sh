cd07de0df50eeb18f1017146c4246174fc1b7952
Thomas <dahlg136@morris.umn.edu>
2021-09-14 17:48:45 -0500

    Add use of pipes to avoid temporary files
    Switched to using pipes instead of making a new file every time.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-cd $1 || exit 1
+cd "$1" || exit 1
 
-cat var/log/secure* > random.txt 
+cat var/log/secure* | \
 
-grep -hr "Failed password for" random.txt > output.txt
+#Gets all the lines that contain the string 'Failed password for' and pipes them to the awk
+grep -hr "Failed password for" | \
 
-awk '{if ($0 ~ /invalid user/) {print $1 " " $2 " " substr($3,1,2) " " $11 " " $13} else {print $1 " " $2 " " substr($3,1,2) " " $9 " " $11}}' output.txt > failed_login_data.txt 
+#If any of the lines have "invalid user" then take the 1st, 2nd, 3rd first 2 elements, 11th, and 13th columns of the text seperated by spaces
+#Else take the columns 1, 2, 3 first 2 characters, 9, and 11 to print out.
+awk '{if ($0 ~ /invalid user/) {print $1 " " $2 " " substr($3,1,2) " " $11 " " $13} else {print $1 " " $2 " " substr($3,1,2) " " $9 " " $11}}' > failed_login_data.txt 
