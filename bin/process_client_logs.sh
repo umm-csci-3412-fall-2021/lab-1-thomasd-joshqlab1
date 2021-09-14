08cf7b916798a8a2383e025c047f25e5d5c5660c
Thomas <dahlg136@morris.umn.edu>
2021-09-14 11:33:37 -0500

    Finish process_client_logs.sh
    Got the method passing the tests, but now need to get rid of output files that clutter user space.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-cd $1
+cd $1 || exit 1
 
-cat *.txt> random.txt
-
+cat var/log/secure* > random.txt 
 
 grep -hr "Failed password for" random.txt > output.txt
 
-sed -e 's/\<cscirepo sshd\>|[[^][]*\]//g' output.txt > failed_login.data.txt
-
-
-
-
+awk '{if ($0 ~ /invalid user/) {print $1 " " $2 " " substr($3,1,2) " " $11 " " $13} else {print $1 " " $2 " " substr($3,1,2) " " $9 " " $11}}' output.txt > failed_login_data.txt 
