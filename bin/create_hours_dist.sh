90b50979b06494c0f5e22321e35c2587627838e6
Thomas <dahlg136@morris.umn.edu>
2021-09-14 19:12:12 -0500

    Add the create_hours_dist.sh file that passes tests
    This file is almost the same as create_username_dist.sh  so we copied it over and changed the necessary fields.

---
+++ bin/create_hours_dist.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Save working directory
+dir=$(pwd)
+
+#CD into given directory
+cd "$1" || exit 1
+
+#Find all the files named failed_login_data.txt and cat them into one file
+find . -type f -name '*failed_login_data.txt' -exec cat {} + | \
+
+#Only grab the column that has the user name
+awk '{print $3}' | \
+
+#Sort the names
+sort | \
+
+#Count how many times each name appears
+uniq -c | \
+
+#Print the names and how many times they appear with data.addRow([])
+awk '{print "data.addRow([\x27"$2"\x27, " $1"]);"}' > temp.txt
+
+#Go back to main
+cd "$dir" || exit 1
+
+#Wrap th data into and html file
+bin/wrap_contents.sh "$1"/temp.txt html_components/hours_dist "$1"/hours_dist.html
+
+#Go back into given directory
+cd "$1" || exit 1
+
+#Remove temp file
+rm temp.txt
