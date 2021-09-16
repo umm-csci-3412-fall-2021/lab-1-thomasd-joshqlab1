99e7b7b9354155fc4b4494099182c3090336f492
Thomas <dahlg136@morris.umn.edu>
2021-09-16 11:32:34 -0500

    Pushing up unfinished work
    Class over

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -3,19 +3,24 @@
 TMPDIR=$(mktemp -d) || exit 1
 
 here=$(pwd)
-
+mkdir random
 for var in "$@"
 do
-    dirname=$var | sed 's/\.zip$//'
-    mkdir random/$dirname
-    tar -xf "$var" -C random/$dirname
+    dirname="${var%.*}"
+    dirname2="${dirname#log_files/}"
+    echo $dirname2
+    cd random
+    mkdir $dirname2
+
+    tar -xf log_files/"$var" $dirname2
     #bin/process_client_logs.sh random/$dirname
+    cd $here
 done
 
 echo $TMPDIR
-bin/create_username_dist.sh random
-bin/create_hours_dist.sh random
-bin/create_country_dist.sh random
-bin/assemble_report.sh random
+#bin/create_username_dist.sh random
+#bin/create_hours_dist.sh random
+#bin/create_country_dist.sh random
+#bin/assemble_report.sh random
 
-mv random/failed_login_summary.html $here
+#mv random/failed_login_summary.html $here
