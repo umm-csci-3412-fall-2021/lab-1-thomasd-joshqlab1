ba206c79a32857472cbdd1fbe2f9f15b89f2234a
Thomas <dahlg136@morris.umn.edu>
2021-09-16 10:08:39 -0500

    Finish assemble_reports.sh
    We finished writing the assemble_reports.sh and it passes all of the tests.

--- bin/assemble_report.sh
+++ bin/assemble_report.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-dir=$(pwd)
-cat "$1"/username_dist.html "$1"/hours_dist.html "$1"/country_dist.html > temp.html
 
-cd "$dir" || exit 1
-bin/wrap_contents.sh temp.html html_components/summary_plots "$1"/failed_login_summary.html
+cat "$1"/country_dist.html "$1"/hours_dist.html  "$1"/username_dist.html> "$1"/temp.html
+
+bin/wrap_contents.sh "$1"/temp.html html_components/summary_plots "$1"/failed_login_summary.html
